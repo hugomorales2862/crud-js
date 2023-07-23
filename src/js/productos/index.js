1db64826bd3af189ca5dda7eff270bac7f466978
Hugo Morales <moralesbatzh@gmail.com>
2023-07-23 05:42:43 -0600

    se mandan a llamar los datos

---
+++ src/js/productos/index.js
@@ -0,0 +1,7 @@
+const formulario = document.querySelector('form')
+const tablaProductos = document.getElementById('tablaProductos');
+const btnBuscar = document.getElementById('btnBuscar');
+const btnModificar = document.getElementById('btnModificar');
+const btnGuardar = document.getElementById('btnGuardar');
+const btnCancelar = document.getElementById('btnCancelar');
+const divTabla = document.getElementById('divTabla');
