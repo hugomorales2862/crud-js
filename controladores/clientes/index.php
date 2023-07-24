719a61e1764d5a2ad63b7628b1e9ced9b58ccdc3
Hugo Morales <moralesbatzh@gmail.com>
2023-07-24 07:58:57 -0600

    se creo el archivo index para los clientes

---
+++ controladores/clientes/index.php
@@ -0,0 +1,54 @@
+<?php
+    require '../../modelos/Cliente.php';
+    $metodo = $_SERVER['REQUEST_METHOD'];
+    $tipo = $_POST['tipo'];
+
+    try{
+        switch($metodo){
+            case 'POST':
+                $cliente =  new Cliente ($_POST);
+                if($tip == 1){
+                    $resultado = $cliente->guardar();
+                    $mensaje = "Se guardo correctamente";
+                }
+                if ($tipo == 2){
+                    $resultado = $cliente->modificar();
+                    $mensaje ="Cliente modificado exitosamente.";
+                }
+                if($tipo == 3){
+                    $resultado = $cliente->eliminar();
+                    $mensaje="Cliente eliminado con éxito.";
+                }
+                if($resultado){
+                    echo json_encode([
+                        'mensaje' => $mensaje,
+                        'codigo' => 1,
+                    ])
+                }else{
+                    echo json_encode([
+                        'mensaje' => $mensaje,
+                        'codigo' => 0,
+
+                    ]);
+                }
+                break;
+            case 'GET':
+                $cliente = new Cliente($_GET);
+                $clientes = $producto->buscar();
+                echo json_encode($clientes);
+                break;
+            default:
+                   echo json_encode([
+                    'mensaje' => 'Metodo no permitido',
+                    'codigo' => 0
+                   ]);
+                   break;
+                }
+
+        }catch(Exception $e){
+            echo json_encode([
+                'detalle' => $e->getMessage(),
+                'mensaje' => 'Ocurrió un error',
+                'codigo' => 0
+            ]);
+        }
